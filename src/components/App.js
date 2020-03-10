6f82ea5e9f7324d39a68a9b989f49536929542b4
funmilayo <funmilayo.anihundidun@venturegardengroup.com>
2020-03-10 10:03:27 +0100

    set up the redux parameters for the application and added other components

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import SearchRecipes from './SeachRecipes';
+import RecipeList from  './RecipeList';
 import '../styles/index.css';
 
 
 class App extends Component {
     render(){
+        console.log('this.props', this.props);
+        
         return(
             <div>
                 <h2>Recipe Finder</h2>
                 <SearchRecipes/>
+                <RecipeList/>
             </div>
         )
     }
--- src/components/SeachRecipes.js
+++ src/components/SeachRecipes.js
@@ -1,27 +1,62 @@
 import React, {Component} from 'react';
 import {Form, FormGroup, FormControl, ControlLabel, Button} from 'react-bootstrap';
+// import axios from 'react';
+import {connect} from 'react-redux';
+import {setRecipes} from '../actions';
 
 
 
 class SearchRecipes extends Component {
+    constructor(){
+        super();
+        this.state={
+            ingredients: "",
+            dish: ""
+        }
+    }
+search() {
+    let {ingredients, dish} = this.state;
+    const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
+
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+      .then(json => {
+          this.props.setRecipes(json.results)
+      });
+}
+
     render(){
         return(
             <Form inline>
                 <FormGroup>
                     <ControlLabel>Ingredients</ControlLabel>
                     {' '}
-                    <FormControl type="text" placeholder="garlic, chicken"/>
+                    <FormControl 
+                    onChange={event => this.setState({ingredients:event.target.value})}
+                    type="text" 
+                    placeholder="garlic, chicken"/>
                 </FormGroup>
                 {' '}
                 <FormGroup>
                     <ControlLabel>Dish</ControlLabel>  
                     {' '}
-                     <FormControl type="text" placeholder="adobo"></FormControl>              
+                     <FormControl 
+                     onChange={event => this.setState({dish:event.target.value})}
+                     type="text"
+                     placeholder="adobo"></FormControl>              
                 </FormGroup>
                 {' '}
-                <Button>Submit</Button>
+                <Button
+                onClick={()=>this.search()}
+                >Submit</Button>
             </Form>
         )
     }
 }
-export default SearchRecipes;
+export default connect(null, {setRecipes})(SearchRecipes);
